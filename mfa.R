26005ac8be40a4983cab98fdcf3e6a520347726f
Jingcheng Xu <jingchengxu@outlook.com>
2016-11-10 19:23:34 -0800

    Add basic MFA functions.

---
+++ mfa.R
@@ -0,0 +1,54 @@
+### Import the data needed for testing
+wines <- read.csv('wines.csv')
+wines <- wines[, 2:(ncol(wines) - 4)]   # Get rid of the last 4 columns
+g <- c(6, 6, 6, 5, 6, 5, 4, 6, 5, 4)
+sets <- list(1:6, 7:12, 13:18, 19:23, 24:29, 30:34, 35:38, 39:44, 45:49, 50:53)
+
+### Create a list of matrices from the 'sets' variable
+split.variables <- function(data, sets) {
+    tables <- list()
+    for (i in 1:length(sets)) {
+        tables[[i]] <- data[, sets[[i]]]
+    }
+
+    if (!is.null(names(sets)))
+        names(tables) <- names(sets)
+    tables
+}
+
+### MFA on normalised tables
+mfa.raw <- function(tables) {
+    for (i in 1:length(tables)) {
+        ## Normalise each table by the singular values
+        sv <- svd(tables[[i]])$d[1]
+        tables[[i]] <- tables[[i]] / sv
+    }
+
+    ## PCA on the whole table
+    X <- do.call(cbind, tables)
+    X.svd <- svd(X)
+    eigenvalues <- X.svd$d ^ 2
+    comp.factor.scores <- X.svd$u %*% diag(X.svd$d)
+    loadings <- X.svd$v
+
+    list(eigenvalues = eigenvalues,
+         factor.scores = comp.factor.scores,
+         factor.loadings = loadings)
+}
+
+mfa <- function(data, sets, ncomp = NULL, center = TRUE, scale = TRUE) {
+    data <- scale(data, center, scale)
+    tables <- split.variables(data, sets)
+    raw <- mfa.raw(tables)
+    nvar <- vapply(tables, ncol, FUN.VALUE = 0) # num of vars in each table
+
+    if (!is.null(ncomp)) {
+        return(list(eigenvalues = raw$eigenvalues[1:ncomp],
+                    factor.scores = raw$factor.scores[, 1:ncomp],
+                    factor.loadings = raw$factor.loadings[, 1:ncomp]
+                    ))
+    }
+
+    raw
+}
+
