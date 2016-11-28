cde78408954a3588fed73a8e84edb6111cb388b4
matthew-rainey <matthew-rainey@users.noreply.github.com>
2016-11-27 23:41:34 -0800

    Create test-mfa-properties.R

---
+++ MFA/tests/testthat/test-mfa-properties.R
@@ -0,0 +1,38 @@
+wines <- read.csv('wines.csv')
+wines <- wines[, 2:(ncol(wines) - 4)]   # Get rid of the last 4 columns
+sets <- list(1:6, 7:12, 13:18, 19:23, 24:29, 30:34, 35:38, 39:44, 45:49, 50:53)
+ncomp <- 5
+test.wines <- mfa(wines, sets, ncomp = ncomp)
+
+test_that("mfa function returns mfa object", {
+  expect_that(test.wines, is_a("mfa"))
+})
+
+test_that("proper number of eigenvalues",{
+  expect_that(test.wines$eig, is_a("numeric"))
+  expect_that(length(test.wines$eig), equals(ncomp))
+})
+
+test_that("dimensions of factor scores matrix",{
+  expect_that(test.wines$factor.scores, is_a("matrix"))
+  expect_that(dim(test.wines$factor.scores), equals(c(nrow(wines), ncomp)))
+})
+
+test_that("dimensons of partial factor scores matrix",{
+  expect_that(test.wines$partial.factor.scores, is_a("list"))
+  expect_that(test.wines$partial.factor.scores[[1]], is_a("matrix"))
+  for (i in 1:length(test.wines$partial.factor.scores)){
+    expect_that(dim(test.wines$partial.factor.scores[[i]]),
+                equals(dim(test.wines$factor.scores)))
+  }
+})
+
+test_that("dimension of loadings matrix",{
+  expect_that(test.wines$loadings, is_a("matrix"))
+  expect_that(dim(test.wines$loadings), equals(c(ncol(wines), ncomp)))
+})
+
+test_that("proper number of weights",{
+  expect_that(test.wines$weights, is_a("numeric"))
+  expect_that(length(test.wines$weights), equals(ncol(wines)))
+})
