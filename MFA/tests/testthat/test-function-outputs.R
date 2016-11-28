f9bb18a11687e318b37622d0f8f3c94c1baf4965
matthew-rainey <matthew-rainey@users.noreply.github.com>
2016-11-27 18:36:43 -0800

    Create test-function-outputs.R

---
+++ MFA/tests/testthat/test-function-outputs.R
@@ -0,0 +1,39 @@
+context("Output from functions have expected properties")
+
+mfa.wines <- mfa(wines, sets)
+
+test_that("Original Data can be obtained from Factor Scores and Loadings",{
+  expect_that(mfa.wines$factor.scores %*% t(mfa.wines$loadings),
+              is_equivalent_to(scale(wines) / sqrt(nrow(mfa.wines$factor.scores) - 1)))
+})
+
+test_that("Loadings and Weights can be used to get the identity matrix",{
+  Q <- t(mfa.wines$loadings)
+  A <- diag(mfa.wines$weights)
+  expect_that(Q %*% A %*% t(Q), is_equivalent_to(diag(nrow(Q))))
+})
+
+test_that("Squared factor squared divided by mass gives the eigen values",{
+  Factor.Eig <- apply(mfa.wines$factor.scores ^ 2 / nrow(mfa.wines$factor.scores),
+                      MARGIN = 2, FUN = sum)
+  expect_that(Factor.Eig, equals(mfa.wines$eig))
+})
+
+test_that("Averaged partial factor scores give the factor scores",{
+  Wines.Factor <- matrix(0, nrow=nrow(mfa.wines$factor.scores),
+                        ncol=ncol(mfa.wines$factor.scores))
+  for (i in 1:length(mfa.wines$partial.factor.scores)) {
+    Wines.Factor <- Wines.Factor + mfa.wines$partial.factor.scores[[i]] /
+                     length(mfa.wines$partial.factor.scores)
+  }
+  expect_that(Wines.Factor, equals(mfa.wines$factor.scores))
+})
+
+test_that("Sum of contributions by the dimensions gives 1",{
+  ctr.obs <- ContribObs(mfa.wines)
+  ctr.var <- ContribVar(mfa.wines)
+  ctr.table <- ContribTable(mfa.wines)
+  expect_that(apply(ctr.obs, MARGIN = 2, FUN = sum), equals(rep(1, ncol(ctr.obs))))
+  expect_that(apply(ctr.var, MARGIN = 2, FUN = sum), equals(rep(1, ncol(ctr.var))))
+  expect_that(apply(ctr.table, MARGIN = 2, FUN = sum), equals(rep(1, ncol(ctr.table))))
+})
