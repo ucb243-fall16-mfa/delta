bb140b9326e0c4f3e423e92f9e0a89069adfdd38
Jingcheng Xu <jingchengxu@outlook.com>
2016-11-28 21:14:07 -0800

    Change tests to refer to new data set.

--- MFA/tests/testthat/test-mfa-properties.R
+++ MFA/tests/testthat/test-mfa-properties.R
@@ -1,8 +1,5 @@
 context("mfa object class and dimensions")
 
-wines <- read.csv('../../../wines.csv')
-wines <- wines[, 2:(ncol(wines) - 4)]   # Get rid of the last 4 columns
-sets <- list(1:6, 7:12, 13:18, 19:23, 24:29, 30:34, 35:38, 39:44, 45:49, 50:53)
 ncomp <- 5
 test.wines <- mfa(wines, sets, ncomp = ncomp)
 
