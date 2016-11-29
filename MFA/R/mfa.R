f29e758e6070eb79c4e428156a5ad02d57b29e20
Jingcheng Xu <jingchengxu@outlook.com>
2016-11-28 22:13:12 -0800

    Divide by the sqrt constant only when scale is TRUE.

--- MFA/R/mfa.R
+++ MFA/R/mfa.R
@@ -56,7 +56,9 @@ NormalizeAndGSVD <- function(tables) {
 mfa <- function(data, sets, ncomp = NULL, center = TRUE, scale = TRUE) {
     var.indeces <- unlist(sets)
     data[ , var.indeces] <-
-        scale(data[ , var.indeces], center, scale) / sqrt(nrow(data) - 1)
+        scale(data[ , var.indeces], center, scale)
+    if (scale)
+        data[ , var.indeces] <- data[ , var.indeces] / sqrt(nrow(data) - 1)
     tables <- SplitTable(data, sets)
     gsvd <- NormalizeAndGSVD(tables)
 
