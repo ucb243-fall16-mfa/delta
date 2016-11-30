0446630c7790d25b249a0b1bed4ad7f6f1118199
Jingcheng Xu <jingchengxu@outlook.com>
2016-11-29 17:45:57 -0800

    Add label to plot functions.

--- MFA/R/plot.R
+++ MFA/R/plot.R
@@ -1,26 +1,27 @@
 #' @import ggplot2
-PlotHelper <- function(data, group, xlab, ylab, title) {
+PlotHelper <- function(data, group, xlab, ylab, title, label) {
     data <- data.frame(data)
     if (!is.null(group)) data <- cbind(data, group)
 
-    plot <- ggplot(data = data) +
+    if (is.null(label)) label <- rownames(data)
+    plot <- ggplot(data = data, aes(x = X1, y = X2, label = label)) +
         labs(x = 'Dimension 1',
              y = 'Dimension 2',
              title = title) +
         coord_fixed(1) +
         geom_hline(yintercept = 0) +
         geom_vline(xintercept = 0)
-        
+
     if (!is.null(group))
-        plot <- plot + geom_point(aes(x = X1, y = X2, colour = group))
+        plot <- plot + geom_text(aes(colour = group))
     else
-        plot <- plot + geom_point(aes(x = X1, y = X2))
+        plot <- plot + geom_text()
     plot
 }
 
-PlotRawFactorScores <- function(fs, group, title) {
+PlotRawFactorScores <- function(fs, group, title, label) {
     PlotHelper(fs, group,
-               'Dimension 1', 'Dimension 2', title)
+               'Dimension 1', 'Dimension 2', title, label)
 }
 
 CheckArg <- function(mfa, dim) {
@@ -32,13 +33,15 @@ CheckArg <- function(mfa, dim) {
 #' @param mfa an object of class "mfa"
 #' @param group a factor vector, indicating which group each row
 #'     belongs to
+#' @param label a character vector, will be used to plot the points,
+#'     default to row numbers when NULL
 #' @param dim a integer vector indicating the dimensions to plot, the
 #'     first element being x and second being y
 #' @export
-PlotFactorScores <- function(mfa, group = NULL, dim = c(1, 2)) {
+PlotFactorScores <- function(mfa, group = NULL, label = NULL, dim = c(1, 2)) {
     CheckArg(mfa, dim)
     fs <- mfa$factor.scores[ , dim]
-    PlotRawFactorScores(fs, group, 'Factor Scores')
+    PlotRawFactorScores(fs, group, 'Factor Scores', label)
 }
 
 #' @title Plot Partial Factor Scores
@@ -46,28 +49,34 @@ PlotFactorScores <- function(mfa, group = NULL, dim = c(1, 2)) {
 #' @param k the partial factor score of which table
 #' @param group a factor vector, indicating which group each row
 #'     belongs to
+#' @param label a character vector, will be used to plot the points,
+#'     default to row numbers when NULL
 #' @param dim a integer vector indicating the dimensions to plot, the
 #'     first element being x and second being y
 #' @export
 PlotPartialFactorScores <- function(mfa, k,
-                                   group = NULL, dim = c(1, 2)) {
+                                    group = NULL, label = NULL,
+                                    dim = c(1, 2)) {
     CheckArg(mfa, dim)
     fs <- mfa$partial.factor.scores[[k]][ , dim]
     title <- paste('Partial factor scores of table', k)
-    PlotRawFactorScores(fs, group, title)
+    PlotRawFactorScores(fs, group, title, label)
 }
 
 #' @title Plot Variable Loadings
 #' @param mfa an object of class "mfa"
 #' @param var.group a factor vector, indicating which group each row
 #'     (variable) belongs to
+#' @param label a character vector, will be used to plot the points,
+#'     default to row numbers when NULL
 #' @param dim a integer vector indicating the dimensions to plot, the
 #'     first element being x and second being y
 #' @export
-PlotLoadings <- function(mfa, var.group = NULL, dim = c(1, 2)) {
+PlotLoadings <- function(mfa, var.group = NULL, label = NULL, dim = c(1, 2)) {
     CheckArg(mfa, dim)
     ld <- mfa$loadings
     PlotHelper(ld, var.group,
                'Dimension 1', 'Dimension 2',
-               'Variable Loadings')
+               'Variable Loadings',
+               label)
 }
