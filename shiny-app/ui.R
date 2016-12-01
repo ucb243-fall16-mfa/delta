ba2d59a80516c8ed468d5c4f4cf86fe9fb59c759
Jingcheng Xu <jingchengxu@outlook.com>
2016-12-01 00:50:20 -0800

    Put conditional panels to sidebar.

--- shiny-app/ui.R
+++ shiny-app/ui.R
@@ -13,27 +13,33 @@ shinyUI(fluidPage(
                                   "Partial Factor Scores" = "partial",
                                   "Loadings" = "loadings",
                                   "Eigenvalues" = "eig"),
-                   selected = "factor")),
+                   selected = "factor"),
+      
+      ## Inputs for which Dimensions to be used
+      conditionalPanel("input.Choice != 'eig'",
+                       numericInput("dim1",
+                                    label = "Dimension 1",
+                                    value = 1,
+                                    min=1,
+                                    max=12),
+                       numericInput("dim2",
+                                    label = "Dimension 2",
+                                    value = 2,
+                                    min=1,
+                                    max=12)
+                       ),
+      
+      ## Conditional Panel to choose the table if partial factor Plot is chosen.
+      conditionalPanel("input.Choice == 'partial'",
+                       numericInput("table",
+                                    label="Table",
+                                    value=1,
+                                    min=1,
+                                    max = 10)
+                       )),
+    
     mainPanel(
-      plotOutput("Plot")
-    )
-  ),
-
-  verticalLayout(
-    ## Inputs for which Dimensions to be used
-    conditionalPanel("input.Choice != 'eig'",
-                     numericInput("dim1",
-                                  label = "Dimension 1",
-                                  value = 1, min=1, max=12),
-                     numericInput("dim2",
-                                  label = "Dimension 2",
-                                  value = 2, min=1, max=12)
-    ),
-    ## Conditional Panel to choose the table if partial factor Plot is chosen.
-    conditionalPanel("input.Choice == 'partial'",
-                     numericInput("table",
-                                  label="Table",
-                                  value=1, min=1, max = 10)
+        plotOutput("Plot")
     )
   )
 ))
