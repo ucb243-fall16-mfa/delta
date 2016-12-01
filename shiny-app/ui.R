8b1215dfda15f9d8e7be560ae91aa76034bc527e
Jingcheng Xu <jingchengxu@outlook.com>
2016-11-30 16:13:46 -0800

    Change the layout of shiny app.

--- shiny-app/ui.R
+++ shiny-app/ui.R
@@ -3,29 +3,34 @@ library(shiny)
 
 shinyUI(fluidPage(
 
-  titlePanel("Wines Shiny App"),
+  titlePanel("MFA demo"),
 
   sidebarLayout(
-    mainPanel(
-      plotOutput("Plot"), width = 12
-    ),
-
     sidebarPanel(
-      ##Buttons to determine what plot to generate
-      radioButtons("Choice", label = h3("Plot Options"),
-                   choices = list("Factor Scores" = "factor", "Partial Factor Scores" = "partial",
-                                  "Loadings" = "loadings"), selected = "factor"),
-      ##Inputs for which Dimensions to be used
+      ## Buttons to determine what plot to generate
+        radioButtons("Choice",
+                     label = h3("Plot Options"),
+                     choices = list("Factor Scores" = "factor",
+                                    "Partial Factor Scores" = "partial",
+                                    "Loadings" = "loadings"),
+                     selected = "factor"),
+      ## Inputs for which Dimensions to be used
       numericInput("dim1",
                    label = "Dimension 1",
                    value = 1, min=1, max=12),
       numericInput("dim2",
                    label = "Dimension 2",
                    value = 2, min=1, max=12)
+    ),
+    
+    mainPanel(
+        plotOutput("Plot")
     )),
-    #Conditional Panel to choose the table if partial factor Plot is chosen.
-    conditionalPanel("input.Choice == 'partial'",
-                     numericInput("table", label="Table",
-                                  value=1, min=1, max = 10)
-    )
-  ))
+  
+  ## Conditional Panel to choose the table if partial factor Plot is chosen.
+  conditionalPanel("input.Choice == 'partial'",
+                   numericInput("table",
+                                label="Table",
+                                value=1, min=1, max = 10)
+                   )
+))
